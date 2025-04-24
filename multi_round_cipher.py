1c450a9726aac49ca4d5ede302a870a979a6d060
ariyoujustcode <arimarine23@gmail.com>
2025-04-24 10:39:21 -0500

    created critical functions

--- multi_round_cipher.py
+++ multi_round_cipher.py
@@ -1,15 +1,7 @@
 import argparse
 
 
-def encrypt(key_file, input_file, output_file, dimension, rounds):
-    print("encrypting")
-
-
-def decrypt(key_file, input_file, output_file, dimension, rounds):
-    print("decrypyting")
-
-
-def main():
+def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Product cipher: encode or decode")
 
     parser.add_argument(
@@ -27,6 +19,56 @@ def main():
         "rounds", type=int, help="Number of rounds for encryption/decryption"
     )
 
+    return parser
+
+
+def read_file(path: str) -> str:
+    with open(path, "r") as file:
+        return file.read()
+
+
+def write_file(path: str, contents: str) -> None:
+    with open(path, "w") as file:
+        file.write(contents)
+
+
+def get_shift_values(key_file) -> list[int]:
+    shift_values = []
+    print("Calculating shift values for vignere cipher...")
+    return shift_values
+
+
+def add_padding(vignere_cipher: str) -> str:
+    padded_vignere_cipher = ""
+    print("Adding padding...")
+    return padded_vignere_cipher
+
+
+def perform_vignere(shift_values: list[int], message: str) -> str:
+    vignere_cipher = ""
+    print("performing vignere...")
+    return vignere_cipher
+
+
+def create_block(vignere_cipher: str, dimension: int) -> list[int]:
+    print("Creating block...")
+
+
+def perform_columnar(block: list[int]) -> str:
+    encrypted_block = ""
+    return encrypted_block
+
+
+def encrypt(key_file, input_file, output_file, dimension, rounds):
+    print("encrypting...")
+
+
+def decrypt(key_file, input_file, output_file, dimension, rounds):
+    print("decrypyting...")
+
+
+def main():
+    parser = get_parser()
     args = parser.parse_args()
 
 
